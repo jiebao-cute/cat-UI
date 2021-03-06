c60c4b3b81ba4e05b9ce35d2bd703f348f99b413
jiebao <908822571@qq.com>
2021-03-06 23:36:41 +0800

    Home页面的CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <h1>cat UI</h1>
       <h2>这是一个厉害的框架</h2>
       <p class="action">
-        <a>gitGub</a>
+        <a>gitHub</a>
         <a>开始</a>
       </p>
     </div>
@@ -18,5 +18,24 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.banner{
+  background: aquamarine;
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  > .action{
+    padding: 10px 0;
+    a{
+      margin: 0 8px;
+      background: lightskyblue;
+      $h: 28px;
+      height: $h;
+      line-height:$h;
+      padding: 0 8px;
+      border-radius: $h/2;
+    }
+  }
+}
 </style>
