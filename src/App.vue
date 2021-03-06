45ddaa084041bebdcfa17fa677eb17cb62506f7a
jiebao <908822571@qq.com>
2021-03-06 16:56:59 +0800

    安装并初始化vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-<div>hi</div>
+<div>导航栏
+|<router-link to = '/'>frank</router-link>
+  |<router-link to = '/xxx'>frank22</router-link>
+</div>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
---
+++ src/components/Frank2.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    我是frank2222
+  </div>
+</template>
+
