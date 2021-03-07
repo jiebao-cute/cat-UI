1622ac51aec32360e48fc164d747c21deae55809
jiebao <908822571@qq.com>
2021-03-07 13:15:09 +0800

    创建一个toggleMenu

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class ="toggleMenu"  @click="toggleMenu"></span>
   </div>
 </template>
 <script lang="ts">
@@ -40,6 +41,17 @@ export default {
       margin: 0 1em;
     }
   }
+  > .toggleMenu{
+
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
   @media (max-width: 500px) {
    > .menu{
      display: none;
@@ -47,6 +59,7 @@ export default {
     > .logo{
       margin: 0 auto;
     }
+    > .toggleMenu{display: inline-block;}
   }
 }
 </style>
