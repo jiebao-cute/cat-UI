6ba3326f2b555a83ee50071fbb6f018a441962ed
jiebao <908822571@qq.com>
2021-03-07 14:26:13 +0800

    doc.vue页面子路由切换后关闭aside

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,18 @@
 
 <script lang="ts">
 import {ref,provide} from 'vue'
+import {router} from './router'
 export default {
   name: 'App',
   setup(){
     const width = document.documentElement.clientWidth;
-    const menuVisible = ref(width > 500);//set
-    provide('menuVisible',menuVisible)
+    const menuVisible = ref(width > 500);
+    provide('menuVisible',menuVisible)//set
+    router.afterEach(()=> {
+      if (width <= 500){
+        menuVisible.value = false
+      }
+    })
   }
 }
 </script>
---
+++ src/components/SwitchDome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    我是switch组件
+  </div>
+</template>
