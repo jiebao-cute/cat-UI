caf1572fcd6a261eff5102534a260b20bee6ac65
jiebao <908822571@qq.com>
2021-03-16 15:14:58 +0800

    创建Tabs和Tab组件

--- src/components/TabsDome.vue
+++ src/components/TabsDome.vue
@@ -1,37 +1,17 @@
-tabs<template>
-  <div>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-    <p>Switch 的文档</p>
-  </div>
+<template>
+  <div>Tabs示例</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航一">内容一</Tab>
+    <Tab title="导航二">内容二</Tab>
+  </Tabs>
 </template>
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+ components:{
+   Tabs,Tab
+ }
+}
+</script>
