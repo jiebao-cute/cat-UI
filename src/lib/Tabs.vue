639415ebdd259bd68e0ca3e637071e46737d997b
jiebao <908822571@qq.com>
2021-03-16 16:00:46 +0800

    如何确认子组件类型（防御性编程）检查context.slots.default()数组

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    Tabs
+    <component :is="defaults[0]"></component>
+    <component :is="defaults[1]"></component>
   </div>
 </template>
+<script lang="ts">
+import Tab from  './Tab.vue'
+export  default {
+  setup(props,contexts){
+     const defaults = contexts.slots.default()
+     defaults.forEach((tag)=>{
+       if (tag.type !== Tab){
+         throw new Error('Tabs 子标签必须是 Tab')
+       }
+    })
+   return{defaults}
+  }
+}
+</script>
