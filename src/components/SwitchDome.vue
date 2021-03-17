e34417fbace7868bdc50afc3dccdcc44184ce42d
jiebao <908822571@qq.com>
2021-03-17 16:46:54 +0800

    将源码表现为五彩斑斓，并且有背景色

--- src/components/SwitchDome.vue
+++ src/components/SwitchDome.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <component :is="Switch1Demo"/>
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre>{{Switch1Demo.__sourceCode}}</pre>
+  <div>
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <component :is="Switch1Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
+      </div>
     </div>
   </div>
 </template>
@@ -16,12 +19,16 @@
 import {ref} from  'vue'
 import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue'
+import  'prismjs';
+import 'prismjs/themes/prism.css'
+const  Prism = (window as any).Prism
+
 export default {
   components:{Button},
   setup(){
     const bool = ref(false)
     return {bool,
-      Switch1Demo}
+      Switch1Demo,Prism}
   }
 }
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -14,7 +14,7 @@ export default {
   }
 }
 </script>
-<style lang="scss" >
+<style lang="scss" scoped>
 $h: 44px;
 $h2: $h - 4px;
 .gulu-switch{
