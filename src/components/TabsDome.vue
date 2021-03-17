7a34786bff0678106819271811ceac3da4c6583d
jiebao <908822571@qq.com>
2021-03-17 19:02:21 +0800

    tabs修改

--- src/components/TabsDome.vue
+++ src/components/TabsDome.vue
@@ -1,22 +1,19 @@
 <template>
-  <div>Tabs示例</div>
-  <h1>示例1</h1>
-  <Tabs v-model:selected = "x">
-    <Tab title="导航一">内容一</Tab>
-    <Tab title="这是一个简单博客">内容二</Tab>
-  </Tabs>
+  <h1>Tabs 标签页</h1>
+  <Demo :component="Tabs1Demo" />
 </template>
+
 <script lang="ts">
-import Tabs from '../lib/Tabs.vue'
-import Tab from '../lib/Tab.vue'
-import {ref} from 'vue'
+import Demo from './Demo.vue'
+import Tabs1Demo from './Tabs1.demo.vue'
 export default {
- components:{
-   Tabs,Tab
- },
-  setup(){
-   const x = ref("这是一个简单博客")
-    return{x}
+  components: {
+    Demo
+  },
+  setup() {
+    return {
+      Tabs1Demo
+    }
   }
 }
 </script>
