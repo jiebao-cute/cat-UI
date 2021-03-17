e1ddd5c57c22eaa8a4511865753efd5bfdb0b9ca
jiebao <908822571@qq.com>
2021-03-17 11:50:22 +0800

    创建多个路由

--- src/router.ts
+++ src/router.ts
@@ -5,13 +5,18 @@ import SwitchDome from './components/SwitchDome.vue'
 import ButtonDome from './components/ButtonDome.vue'
 import DialogDome from './components/DialogDome.vue'
 import TabsDome from './components/TabsDome.vue'
-
+import Intro from  './views/Intro.vue'
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
 const history = createWebHashHistory()
 export const router = createRouter({
   history: history,
   routes: [
     {path: '/', component: Home},
     {path: '/doc', component: Doc,children:[
+        {path:'intro',component:Intro},
+        {path:'get-started',component:GetStarted},
+        {path:'install',component:Install},
         {path:'switch',component:SwitchDome},
         {path:'button',component:ButtonDome},
         {path:'dialog',component:DialogDome},
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,19 +3,31 @@
     <Topnav :toggle-menu-button-visible="true" class="nav"/>
     <div class="content">
       <aside v-if="menuVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始使用</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/switch">Switch</router-link>
+            <router-link to="/doc/switch">Switch 组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/button">Button组件</router-link>
+            <router-link to="/doc/button">Button 组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/dialog">Dialog组件</router-link>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/tabs">Tabs组件</router-link>
+            <router-link to="/doc/tabs">Tabs  组件</router-link>
           </li>
         </ol>
       </aside>
