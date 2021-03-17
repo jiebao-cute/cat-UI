047a014eca47c632e6f6852c2df2305f3cef8679
jiebao <908822571@qq.com>
2021-03-18 02:27:39 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export {default as Switch} from './Switch.vue'//把button的默认值导出为button
+export {default as Button} from './Button.vue'
+export {default as Tabs} from './Tabs.vue'
+export {default as Tab} from './Tab.vue'
+export {default as Dialog} from './Dialog.vue'
+export {openDialog as openDialog} from './openDialog'
