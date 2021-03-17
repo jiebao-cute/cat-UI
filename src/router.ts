0b56e2420b241802344f027e35e5f6bdaadeb234
jiebao <908822571@qq.com>
2021-03-17 19:02:39 +0800

    修改一进入界面的路由

--- src/router.ts
+++ src/router.ts
@@ -14,6 +14,7 @@ export const router = createRouter({
   routes: [
     {path: '/', component: Home},
     {path: '/doc', component: Doc,children:[
+        {path:'',redirect:'/doc/Intro'},
         {path:'intro',component:Intro},
         {path:'get-started',component:GetStarted},
         {path:'install',component:Install},
